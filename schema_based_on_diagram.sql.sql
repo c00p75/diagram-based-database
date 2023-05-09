59649d751b001e4a673d04636ea194dae2c09ee7
Kelvin <ngotho.kelvin@yahoo.com>
2023-05-09 18:50:47 +0300

    Add invoice & invoice_items tables

--- schema_based_on_diagram.sql.sql
+++ schema_based_on_diagram.sql.sql
@@ -1,15 +1,34 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE medical_histories (
-    id INT PRIMARY KEY NOT NULL,
-    admitted_at TIMESTAMP,
-    patient_id INT, 
-    status VARCHAR
-);
-
 CREATE TABLE patients (
     id INT PRIMARY KEY NOT NULL, 
     name VARCHAR,
-    date_of_birth DATE,
-    FOREIGN KEY (id) REFERENCES medical_histories (id)
+    date_of_birth DATE
+);
+
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY NOT NULL,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients (id), 
+    status VARCHAR
+);
+
+
+
+CREATE TABLE invoices (
+    id INT PRIMARY KEY NOT NULL, 
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_histories REFERENCES medical_histories (id)
+);
+
+
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY NOT NULL, 
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices (id),
+    treatment_id INT REFERENCES treatments (id)
 );
